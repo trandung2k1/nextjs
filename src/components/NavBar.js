87287d65d26a48d5bb6be3b7a630568dc53f4994
trandung2k1 <trandungitksnb2001@gmail.com>
2023-11-11 11:04:40 +0700

    update version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,40 +2,40 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
 const NavBar = () => {
-    return (
-        <>
-            <Navbar>
-                <Link href="/" passHref>
-                    <Navbar.Brand>Home</Navbar.Brand>
-                </Link>
-                <Nav>
-                    <Nav.Item>
-                        <Link href="/about" passHref>
-                            About
-                        </Link>
-                    </Nav.Item>
-                    &nbsp;&nbsp;
-                    <Nav.Item>
-                        <Link href="/todos" passHref>
-                            Todos
-                        </Link>
-                    </Nav.Item>
-                    &nbsp;&nbsp;
-                    <Nav.Item>
-                        <Link href="/jokes/random" passHref>
-                            Jokes
-                        </Link>
-                    </Nav.Item>
-                    &nbsp;&nbsp;
-                    <Nav.Item>
-                        <Link href="/books" passHref>
-                            Books
-                        </Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar>
+        <Link href="/" passHref>
+          <Navbar.Brand>Home</Navbar.Brand>
+        </Link>
+        <Nav>
+          <Nav.Item>
+            <Link href="/about" passHref>
+              About
+            </Link>
+          </Nav.Item>
+          &nbsp;&nbsp;
+          <Nav.Item>
+            <Link href="/todos" passHref>
+              Todos
+            </Link>
+          </Nav.Item>
+          &nbsp;&nbsp;
+          <Nav.Item>
+            <Link href="/jokes/random" passHref>
+              Jokes
+            </Link>
+          </Nav.Item>
+          &nbsp;&nbsp;
+          <Nav.Item>
+            <Link href="/books" passHref>
+              Books
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+    </>
+  );
 };
 
 export default NavBar;
--- src/pages/todos/[id].js
+++ src/pages/todos/[id].js
@@ -7,55 +7,50 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useRouter } from 'next/router';
 import spinnerStyles from '../../styles/Spinner.module.css';
 const TodoItem = ({ todo }) => {
-    const router = useRouter();
-    if (router.isFallback) {
-        return (
-            <Spinner
-                animation="border"
-                role="status"
-                variant="dark"
-                className={spinnerStyles.spinnerLg}
-            >
-                <span className="sr-only">Loading...</span>
-            </Spinner>
-        );
-    }
+  const router = useRouter();
+  if (router.isFallback) {
     return (
-        <Layout>
-            <Card className="my-3 shadow">
-                <Card.Body>
-                    <Card.Title>{todo?.title}</Card.Title>
-                    {/* <Card.Text>{todo.body}</Card.Text> */}
-                    <Link href="/todos">
-                        <Button variant="dark">Back</Button>
-                    </Link>
-                </Card.Body>
-            </Card>
-        </Layout>
+      <Spinner animation="border" role="status" variant="dark" className={spinnerStyles.spinnerLg}>
+        <span className="sr-only">Loading...</span>
+      </Spinner>
     );
+  }
+  return (
+    <Layout>
+      <Card className="my-3 shadow">
+        <Card.Body>
+          <Card.Title>{todo?.title}</Card.Title>
+          {/* <Card.Text>{todo.body}</Card.Text> */}
+          <Link href="/todos">
+            <Button variant="dark">Back</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Layout>
+  );
 };
 export const getStaticPaths = async () => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3');
-    const paths = data.map((todo) => ({
-        params: {
-            id: `${todo.id}`,
-        },
-    }));
-    console.log(paths);
-    // const paths = data.map((todo) => `/todos/${todo.id}`);
-    //console.log(paths); //[ '/todos/1', '/todos/2', '/todos/3' ]
-    return {
-        paths: paths,
-        fallback: true,
-    };
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3');
+  const paths = data.map((todo) => ({
+    params: {
+      id: `${todo.id}`,
+    },
+  }));
+  // console.log(paths);
+  // const paths = data.map((todo) => `/todos/${todo.id}`);
+  //console.log(paths); //[ '/todos/1', '/todos/2', '/todos/3' ]
+  return {
+    paths: paths,
+    fallback: true,
+  };
 };
 export const getStaticProps = async ({ params }) => {
-    console.log('getStaticProps', params);
-    const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${params.id}`);
-    return {
-        props: {
-            todo: res.data,
-        },
-    };
+  // console.log('getStaticProps', params);
+  const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${params.id}`);
+  return {
+    props: {
+      todo: res.data,
+    },
+  };
 };
 export default TodoItem;
